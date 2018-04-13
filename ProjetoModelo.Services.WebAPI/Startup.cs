06872b69d0d511bb0fc434c1a32ed6bedad63968
pramos <pramos@NT-PRAMOS>
2018-04-13 14:59:40 -0300

    Add project files.

---
+++ ProjetoModelo.Services.WebAPI/Startup.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+
+namespace ProjetoModelo.Services.WebAPI
+{
+    [assembly: OwinStartup(typeof(Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+         
+        }
+    }
+}
